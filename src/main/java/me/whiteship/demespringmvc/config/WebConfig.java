078e14f216fe988ff1a6b8b604ae65664a75273d
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2023-01-05 23:24:34 +0900

    스프링 웹 MVC 4부: 정적 리소스 지원

--- src/main/java/me/whiteship/demespringmvc/config/WebConfig.java
+++ src/main/java/me/whiteship/demespringmvc/config/WebConfig.java
@@ -1,9 +1,15 @@
 package me.whiteship.demespringmvc.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/m/**")
+                .addResourceLocations("classpath:/m/")
+                .setCachePeriod(20);
+    }
 }
